bf9dda0fc6fddf5e060565e2513a1838e2e05180
RosaliaGroup <ana@rosaliagroup.com>
2026-03-09 22:41:48 -0400

    Fix calendar ID in book.js

--- netlify/functions/book.js
+++ netlify/functions/book.js
@@ -3,7 +3,7 @@ const { google } = require('googleapis');
 // Client configurations
 const CLIENTS = {
   rosalia: {
-    calendarId: 'inquiries@rosaliagroup.com',
+    calendarId: '4fcabed77eab22c25e9ff8440251d5836faaa66b7f8164b94134d439fab62398@group.calendar.google.com',
     notifyPhone: '+16462269189',
     notifyName: 'Ana',
     teamName: 'Rosalia Group',
