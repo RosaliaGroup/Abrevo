241418c6f883890724cce6e75de10fc0de687998
RosaliaGroup <ana@rosaliagroup.com>
2026-03-11 22:01:34 -0400

    Rebuild book.js - fix time, email, team SMS, calendar

--- netlify/functions/book.js
+++ netlify/functions/book.js
@@ -3,8 +3,8 @@ const { google } = require('googleapis');
 // Client configurations
 const CLIENTS = {
   rosalia: {
-    calendarId: '4fcabed77eab22c25e9ff8440251d5836faaa66b7f8164b94134d439fab62398@group.calendar.google.com',
-    notifyPhone: '+12014970225',
+    calendarId: 'inquiries@rosaliagroup.com',
+    notifyPhone: '+16462269189',
     notifyName: 'Ana',
     teamName: 'Rosalia Group',
     googleCredentials: JSON.parse(process.env.GOOGLE_CREDENTIALS || '{}'),
