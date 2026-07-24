056ad16853abddc8e0d80c2f6e0dcd773d9fad76
RosaliaGroup <ana@rosaliagroup.com>
2026-07-24 19:33:33 -0400

    Add SMS module and send-link tool for Rosalia
    Add Telnyx SMS module and Vapi send-link tool for Rosalia assistants.

--- functions/lib/sms.js
+++ functions/lib/sms.js
@@ -1,1 +1,14 @@
-﻿Get-Clipboard -Raw | Set-Content functions\lib\sms.js -Encoding utf8
+Abrevo/functions
+/send-link.js
+RosaliaGroup
+RosaliaGroup
+Add Telnyx SMS module and Vapi send-link tool for Rosalia assistants
+07b44a3
+ · 
+2 minutes ago
+
+Code
+
+Blame
+1 lines (1 loc) · 74 Bytes
+﻿Get-Clipboard -Raw | Set-Content functions\send-link.js -Encoding utf8
